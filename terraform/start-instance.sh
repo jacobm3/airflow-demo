3ed4ecc6a84fd44d81d56d8e5e76567296ef8780
Jacob Martinson <jacobm3@gmail.com>
2022-05-20 11:02:08 -0500

    Add excalidraw diagrams.

--- terraform/start-instance.sh
+++ terraform/start-instance.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -x 
+
 aws ec2 start-instances --instance-ids $(terraform output --raw airflow_instance_id)
 aws ec2 start-instances --instance-ids $(terraform output --raw vault_instance_id)
 
