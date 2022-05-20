fa7bdb9dd10f44bd2f6a43832416b3ba1dced219
Jacob Martinson <jacobm3@gmail.com>
2022-05-19 20:06:33 -0500

    Update

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,11 +2,20 @@ provider "namecheap" {
   use_sandbox = false
 }
 
-resource "namecheap_record" "foobar" {
+resource "namecheap_record" "airflow" {
   domain     = "theneutral.zone"
   name       = "airflow"
   address    = aws_instance.web.public_ip
   type       = "A"
   ttl        = "60"
   depends_on = [aws_instance.web]
+}
+
+resource "namecheap_record" "vault" {
+  domain     = "theneutral.zone"
+  name       = "vault"
+  address    = aws_instance.vault.public_ip
+  type       = "A"
+  ttl        = "60"
+  depends_on = [aws_instance.vault]
 }
--- terraform/start-instance.sh
+++ terraform/start-instance.sh
@@ -1,4 +1,5 @@
 aws ec2 start-instances --instance-ids $(terraform output --raw airflow_instance_id)
+aws ec2 start-instances --instance-ids $(terraform output --raw vault_instance_id)
 
 sleep 5
 
